e4a40223c4ed6e9b12f5ad85522f9520eb52d063
tanisha <Tanishanigam3@gmail.com>
2020-05-14 00:38:55 +0530

    initial

---
+++ adding complex.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a ,b ,c,d;
+	cin<<"enter integer part of complex no."<<a<<b;
+	cin<<"enter complex part of complex no"<<c<<d;
+	int s1=a+b;
+	int s2=c+d;
+	cout>>s1<<s2"i">>endl;
+}
---
+++ average.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+main()
+{
+	int a=5;
+	int b=10;
+	int c=15;
+	int d=20;
+	int e=a+b+c+d;
+	int g=e/4;
+	printf("average of 5 nos.");
+		printf("%d",g);
+}
